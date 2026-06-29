a08ab567a0f58b6c7f5cd9eff8f86529e4aa9ab8
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-06-29 17:38:45 +0200

    Create __init__.py with knowledge providers info
    Added docstring for knowledge providers module.

---
+++ knowledge/providers/__init__.py
@@ -0,0 +1,15 @@
+"""
+DeDe - Knowledge Providers
+
+Knowledge providers retrieve external or internal knowledge.
+
+Current provider:
+- LocalProvider
+
+Future providers:
+- WebProvider
+- LLMProvider
+- WikipediaProvider
+- PDFProvider
+- ScientificDatabaseProvider
+"""
