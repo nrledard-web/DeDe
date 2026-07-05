14c2fd98f47c9029bfdc62ff1f123e91c37740a8
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-07-05 23:27:46 +0200

    Add SearchProvider class with search method stub

--- search/search_provider.py
+++ search/search_provider.py
@@ -1,1 +1,16 @@
+"""
+DeDe - Search Provider Interface
+"""
 
+from typing import Any
+
+
+class SearchProvider:
+    name = "base_provider"
+
+    def search(
+        self,
+        query: str,
+        max_results: int = 5,
+    ) -> dict[str, Any]:
+        raise NotImplementedError
