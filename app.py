114be1826465c432b099eff85bfd22c18ca7d22c
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-07-13 06:04:51 +0200

    Update description of Cognitive AI Governor capabilities

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ st.markdown(
         font-weight:400;
         margin-top:0;
         margin-bottom:0.3rem;">
-        The first Cognitive AI Governor with Evolving Multi-AI and Multi-Tool Capabilities
+        The first Cognitive AI Governor with Evolving and interchangeable Multi-AI and Multi-Tool Capabilities
     </h4>
 
     <p style="
