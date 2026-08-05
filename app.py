5690b1c357d11e4f531a1c9c8eb06180d2154ebb
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-08-05 12:24:28 +0200

    Update title to reflect new agent description

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ st.markdown(
         font-weight:400;
         margin-top:0;
         margin-bottom:0.3rem;">
-        The First Evolving, Human-Like Cognitive AI Governor — Powered by Combinable AI Models and Tools
+        The First Agent, Human-Like Cognitive AI Governor — Powered by Combinable AI Models and Tools
     </h4>
 
     <p style="
