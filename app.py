3bcfa99fb0fbe7bd4d7597eee0b048db8c6b8ff1
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-08-06 16:41:42 +0200

    Update header text in app.py

--- app.py
+++ app.py
@@ -209,6 +209,7 @@ st.markdown(
         margin-top:0;
         margin-bottom:0.3rem;">
         The First Agentic, Human-Like Cognitive AI Governor<br>
+        With User-Owned, Private, and Portable Memory Storage<br>
         Powered by Combinable AI Models and Tools<br>
         The only Agent AI with a Built-In Anti-Coherence Loop<br>
         To Prevent Self-Reinforcing Reasoning and Cognitive Closure.
